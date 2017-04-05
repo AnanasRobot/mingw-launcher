4796b9349633e12d6cca0435a0998ea48efb50d2
Daniel Dulaney <dulaney.daniel@gmail.com>
2017-04-05 02:27:16 -0400

    Basic MinGW bin folder calculation
    Currently, it's just <working_dir>\mingw\bin

--- launch.c
+++ launch.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#define MAX_PATH_LEN 255
 
 int main()
 {
 	printf("Launching MinGW console...\n");
 
-	system("echo %path%");
+	char path_entry[MAX_PATH_LEN];		// Absolute path to the MinGW binaries
+	getcwd(path_entry, MAX_PATH_LEN);	// Start with the current working dir
+	strcat(path_entry, "\\mingw\\bin");	// Assume a standard directory structure
+
+	printf("MinGW binaries path: %s\n", path_entry);
+
 }
